b53a024e83b6f0415a57c7d89f5ff205cf2e0489
tomomasauchida <tomomasa.is.0930@gmail.com>
2022-11-23 11:28:09 +0900

    feat: add chapter15

---
+++ chapter15/go.mod
@@ -0,0 +1,5 @@
+module github.com/tom-uchida/go_todo_app
+
+go 1.17
+
+require golang.org/x/sync v0.1.0
---
+++ chapter15/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	// 実行時引数でポート番号を指定できるようにする。
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
+		log.Printf("failed to  terminate server: %v", err)
+		os.Exit(1)
+	}
+}
+
+// *http.Server.ListenAndServeメソッドでHTTPリクエストを受け付けながら、
+// 引数で受け取ったcontext.Context型の値を経由して外部からの終了通知を待機する。
+func run(ctx context.Context, l net.Listener) error {
+	s := &http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+		}),
+	}
+	eg, ctx := errgroup.WithContext(ctx)
+	// 別ゴルーチンでHTTPサーバーを起動する
+	eg.Go(func() error {
+		// ListenAndServeメソッドではなく、Serveメソッドに変更する。
+		if err := s.Serve(l); err != nil &&
+			// http.ErrServerClosedは
+			// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// チャネルからの通知(終了通知)を待機する
+	<-ctx.Done()
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+	// Goメソッドで起動した別ゴルーチンの終了を待つ。
+	return eg.Wait()
+}
